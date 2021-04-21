82be54c64e837a189e3660b641d2fe0479da4ee7
Gerard Llorach <gerard.llorach@gmail.com>
2021-04-21 16:24:31 +0200

    freq sizes with adding species

--- VISAP/js/tamany.js
+++ VISAP/js/tamany.js
@@ -8,62 +8,50 @@ let count = 0;
 export const startTamany = () => {
 
   // Load information
-  getSizes('http://localhost:8080/sizes', 'data/sizes.json', createApp);
+  getSizes('http://localhost:8080/sizes', 'data/sizes.json', createGraphInterface);
 
 }
 
 // Processes the data and creates the chart
-const createApp = (inData) => {
+const createGraphInterface = (inData) => {
 
-  // Process data
+  // Remove loader and show interface
+  document.getElementById('loader').remove();
+  const interfaceEl = document.getElementById('tamanyInterface');
+  interfaceEl.style.visibility = "visible";
+
+  // Store data
   dataFromServer = inData;
 
+  // Selection species list to add plots
   let species = getUnique(inData, "NomEspecie"); // Useful to create species selector
+  const listEl = document.getElementById('listSpecies');
+  listEl.remove(); // Hide intially the list with species
+  createSearchList(inData, species, listEl);
 
+
+  // Get data for a specific species for first chart
   let dataSpeciesForGraph = getDataForSpecieX(dataFromServer, species[0])
-
-  // TODO in chart configuration
-  /*xAxis: {
-      categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
-      tickmarkPlacement: 'on',
-      title: {
-          enabled: false
-      }
-  },*/
   // Create Highchart
   myChart = createChart(dataSpeciesForGraph);
-  //myChart.addSeries(graphSpecies);
-  // Escull la especie button dropdown
-
-
-
-  let graph2= {
-    name: 'Gamba rosada 2',
-    data:
-      [null, null, null, null, null, null, null, null, null, null,
-            5, 25, 50, 120, 150, 200, 426, 660, 869, 1060,
-            1605, 2471, 3322, 4238, 5221, 6129, 7089, 8339, 9399, 10538,
-            11643, 13092, 14478, 15915, 17385, 19055, 21205, 23044, 25393, 27935,
-            30062, 32049, 33952, 35804, 37431, 39197, 45000, 43000, 41000, 39000,
-            37000, 35000, 33000, 31000, 29000, 27000, 25000, 24000, 23000, 22000,
-            21000, 20000, 19000, 18000, 18000, 17000, 16000, 15537, 14162, 12787,
-            12600, 11400, 5500, 4512, 4502, 4502, 4500, 4500
-      ]
-  };
-
-
-
   //myChart.showLoading();
-  const addSerieBtnEl = myChart.container.querySelector('[aria-label="Show +"]');
-  const removeSerieBtnEl = myChart.container.querySelector('[aria-label="Show -"]');
+  //myChart.addSeries(graphSpecies);
+
+
+  // Button events
+  const addSerieBtnEl = interfaceEl.querySelector('#addSpecies');
+  const removeSerieBtnEl = interfaceEl.querySelector('#deleteSpecies');
+
+
   addSerieBtnEl.addEventListener('click', () => {
-    count++;
-    dataSpeciesForGraph = getDataForSpecieX(dataFromServer, species[count])
-    myChart.addSeries(dataSpeciesForGraph);
+    if (listEl.parentElement === null) // If it is not already there
+      addSerieBtnEl.parentElement.appendChild(listEl);
   });
   removeSerieBtnEl.addEventListener('click', () => {
-    if (myChart.series.length > 3)
+    if (myChart.series.length > 0)
       myChart.series[myChart.series.length-1].remove()
+    if (listEl.parentElement !== null) // Remove
+      listEl.remove();
   });
 
 
@@ -110,17 +98,17 @@ const createChart = (serieSpecies) => {
         //    enabled: false
         //}
     },
-    /*xAxis: {
+    xAxis: {
         allowDecimals: false,
         labels: {
             formatter: function () {
                 return this.value + ' cm'; // clean, unformatted number for year
             }
         },
-        accessibility: {
+        /*accessibility: {
             rangeDescription: 'Range: 1cm to 10cm'//'Range: 1940 to 2017.'
-        }
-    },*/
+        }*/
+    },
     yAxis: {
         title: {
             text: 'Nombre d\'individus'//'Nuclear weapon states'
@@ -139,13 +127,7 @@ const createChart = (serieSpecies) => {
       showDuration: 500
     },
     series: [
-      {
-          name: '+',
-          data: [null, null, null, null, null, null, null, null, null]
-      }, {
-          name: '-',
-          data: [null, null, null, null, null, null, null, null, null]
-      }, serieSpecies
+       serieSpecies
       ],
     plotOptions: {
         area: {
@@ -167,6 +149,57 @@ const createChart = (serieSpecies) => {
   return hChart;
 }
 
+
+
+// Create list selector // https://listjs.com/api/
+const createSearchList = (inData, speciesArray, listEl) => {
+
+  // Create values for each species
+  let values = [];
+  speciesArray.forEach(item => {
+    // Get color from palette
+    let colorItem = palette[item] === undefined ? "rgb(" + palette.Altres.color + ")" : "rgb(" + palette[item].color + ")";
+    // Get common name
+    let commonName = "";
+    // Find common name in data (not optimal)
+    for (let i = 0; i<inData.length; i++){
+      if (inData[i].NomEspecie == item && inData[i].NomComu !== null){
+        commonName = inData[i].NomComu;
+        break;
+      }
+    }
+    // Assign for the List
+    values.push({name: item, color: colorItem, nomComu: commonName}); //  Add the common name for the search?//});
+  });
+  // Create HTML template
+  let options = {
+    item: (values) => "<li'><button class='btn btn-default' style='text-align:left'>"+
+    "<span style='color:"+ values.color +"'> ● </span>" +
+    values.name +
+    "</button></li>"
+  }
+
+  // Create HTML
+  let listSpecies = new List(listEl, options, values);
+
+  // Assign button events
+  listSpecies.list.childNodes.forEach((el)=>el.addEventListener("click", (e)=>{
+    // Get species name
+    let speciesName = e.currentTarget.innerText.split("● ")[1];
+    // Unselect all others
+
+
+    // Add data series
+    let dataSpeciesForGraph = getDataForSpecieX(inData, speciesName)
+    myChart.addSeries(dataSpeciesForGraph);
+    // Hide element
+    listEl.remove();
+  }));
+}
+
+
+
+
 // Get sizes (freqüència talles)
 // var results = fetch("http://localhost:8080/sizes").then(r => r.json()).then(r => results = r).catch(e => console.log(e))
 const getSizes = async (address, staticUrl, callbackPrepareData) => {
@@ -187,24 +220,29 @@ const getSizes = async (address, staticUrl, callbackPrepareData) => {
 }
 
 
+
 // Prepares the data for the highchart
 const getDataForSpecieX = (inData, inSpecies) => {
   // Select the data from a species
   let dataSelSpecies = inData.filter((item) => item.NomEspecie == inSpecies);
-  // Categories
+  // Categories (sizes)
   let categories = getUnique(dataSelSpecies, "Talla"); // Important to create X axis
   categories.forEach((cat, index) => categories[index] = parseFloat(cat)); // Transform into numbers
   categories.sort((a, b) => a - b); // Sort
-  // Sizes
+  // Number of individuals per size
   let numInd = getNumIndPerCategories(dataSelSpecies, categories);
   // Prepare for highcharts
   let dataHC = [];
-  categories.forEach((catItem, index) => dataHC[index] = [catItem, numInd[index]]);
-  console.log(dataHC);
+  categories.forEach((catItem, index) => dataHC[index] = [catItem, numInd[index]]); // Make an array as [categoryA,numIndInA], [categoryB,numIndInB],...
   // https://www.highcharts.com/demo/spline-irregular-time
-  let graphSpecies = {name: inSpecies, data: dataHC};
+
+  // Add common name if exists
+  let seriesName = dataSelSpecies[0].NomComu === null ? inSpecies : (inSpecies + " ("+ dataSelSpecies[0].NomComu +")");
+  let seriesColor = palette[inSpecies] === undefined ? "rgb(" + palette.Altres.color + ")" : "rgb(" + palette[inSpecies].color + ")";
+  let graphSpecies = {name: seriesName, data: dataHC, color: seriesColor };
   return graphSpecies;
 }
+
 
 
 
@@ -221,7 +259,8 @@ const getUnique = (inData, inKey) => {
 }
 
 
-// Select data
+
+// Select data per specie
 const selectData = (inData, especie) => {
   let outData = [];
 
@@ -229,15 +268,13 @@ const selectData = (inData, especie) => {
   for (let i = 0; i < dataFromServer.length; i++){
     if (inData[i].NomEspecie == especie)
       outData.push(inData[i]);
-      /*outData.push({
-        num: inData[i].NIndividus,
-        size: inData[i].Talla
-      });*/
-      //outData[inData[i].Talla] = parseInt(inData[i].NIndividus);
   }
 
   return outData;
 }
+
+
+
 
 // Get sizes given the categories
 const getNumIndPerCategories = (inData, categories) => {
