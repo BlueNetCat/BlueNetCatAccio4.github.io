be630cc9871fa3d81e9634b52ecdd50083f69760
gerardllorach <gerard.llorach@gmail.com>
2021-07-15 21:17:21 +0200

    line trail still not working

--- geoportal/map_utils/animation.js
+++ geoportal/map_utils/animation.js
@@ -3,15 +3,65 @@
 
 // VARIABLES
 // Reusable arrays for optimization
-let seaVelVec2 = [undefined, undefined];
-let pixelVec2 = [undefined, undefined];
-let particles = [];
+let seaVelVec2 = [0,0];
+let pixelVec2 = [0,0];
+// Canvas
 let canvasParticles;
 let ctx;
-
-let numParticles = 1000;
+// Particle animation
+let numParticles = 10000;
 let numVerticesPath = 10;
 let vertices = new Float32Array(numParticles*numVerticesPath*2);
+let lifeParticle = new Float32Array(numParticles);
+let velocityVertices = new Float32Array(numParticles*numVerticesPath);
+let speedFactor = 0.1;
+
+// Data
+let imgDataEast;
+let imgDataNorth;
+let imgWidth;
+let imgHeight;
+// Application state
+let loading = 0;
+let loaded = 0;
+let prevTime = 0;
+
+// When layer has been loaded, update image
+seaVelocityEastLayer.getSource().on('tileloadend', function () {
+  loaded++;
+  // Only update when all tiles are loaded
+  if (loading == loaded){
+    getDataFromLayers();
+  }
+});
+seaVelocityNorthLayer.getSource().on('tileloadend', function () {
+  loaded++;
+  // Only update when all tiles are loaded
+  if (loading == loaded){
+    getDataFromLayers();
+  }
+});
+// When layer starts loading because of map change
+seaVelocityEastLayer.getSource().on('tileloadstart', function () {
+  imgDataEast = undefined;
+  loading++;
+});
+seaVelocityNorthLayer.getSource().on('tileloadstart', function () {
+  loading++;
+  imgDataNorth = undefined;
+});
+// Get the data from layers once loading has finished
+const getDataFromLayers = () => {
+  let tmpCnv = seaVelocityEastLayer.getRenderer().getImage();
+  imgDataEast = tmpCnv.getContext("2d").getImageData(0,0,tmpCnv.width,tmpCnv.height);
+  tmpCnv = seaVelocityNorthLayer.getRenderer().getImage();
+  imgDataNorth = tmpCnv.getContext("2d").getImageData(0,0,tmpCnv.width,tmpCnv.height);
+  imgWidth = tmpCnv.width;
+  imgHeight = tmpCnv.height;
+  repositionParticles();
+  update();
+}
+
 
 
 
@@ -19,38 +69,37 @@ let vertices = new Float32Array(numParticles*numVerticesPath*2);
 // Get the sea velocity at a given pixel
 const getSeaVelAtPixel = (pixel, seaVelVec2) => {
   seaVelVec2[0] = undefined; seaVelVec2[1] = undefined; // Reset seaVelVec2
-  // Get value at pixel
-  map.forEachLayerAtPixel(pixel, (layer, pixel) => {
-    if (layer.getClassName() === 'seaVelocityEast')
-      seaVelVec2[0] = (seaVelocityColorRange[1]-seaVelocityColorRange[0]) * pixel[0]/255 + seaVelocityColorRange[0]; // Normalize;
-    else if (layer.getClassName() === 'seaVelocityNorth')
-      seaVelVec2[1] = (seaVelocityColorRange[1]-seaVelocityColorRange[0]) * pixel[0]/255 + seaVelocityColorRange[0]; // Normalize;
+  pixel[0] = Math.round(pixel[0]);
+  pixel[1] = Math.round(pixel[1]);
 
-  }, {
-    layerFilter: ll => (ll === seaVelocityEastLayer || ll === seaVelocityNorthLayer)
-  });
-  return seaVelVec2;
-}
-
-
-
-
-
-const createParticles = (inNumParticles) => {
-  particles = [];
-  numParticles = inNumParticles || 1000;
-  //let numVerticesPath = 10;
-
-  for (var i = 0; i< numParticles; i++){
-    particles[i] = {
-      vertices: new Float32Array(numVerticesPath*2),
-      life: 0,
-    };
+  if (!imgDataEast || !imgDataNorth){
+    //console.log("data not loaded");
+    return seaVelVec2;
   }
+
+  let imgArrayPos = (pixel[0] + pixel[1]*imgWidth) * 4; // + 1,2,3 if you want (R)GBA
+  let eastValue = imgDataEast.data[imgArrayPos];
+  let northValue = imgDataNorth.data[imgArrayPos];
+
+  if (imgDataEast.data[imgArrayPos + 3] == 0){ // No alpha, no data
+    return seaVelVec2;
+  }
+
+  // Assign to vector
+  seaVelVec2[0] = (seaVelocityColorRange[1]-seaVelocityColorRange[0]) * eastValue/255 + seaVelocityColorRange[0]; // Normalize;
+  seaVelVec2[1] = (seaVelocityColorRange[1]-seaVelocityColorRange[0]) * northValue/255 + seaVelocityColorRange[0]; // Normalize;
+
+  return seaVelVec2;
+
 }
 
 
-const updateParticles = () => {
+
+
+
+
+// Repositions all particles and paths
+const repositionParticles = () => {
   //for (var i = 0; i<particles.length; i++){
   for (var i = 0; i< numParticles; i++){
 
@@ -58,13 +107,14 @@ const updateParticles = () => {
     generatePoint(pixelVec2, seaVelVec2);
 
     // Assign initial value
-    //particles[i].vertices[0] = pixelVec2[0];
-    //particles[i].vertices[1] = pixelVec2[1];
     vertices[i*numVerticesPath*2] = pixelVec2[0];
     vertices[i*numVerticesPath*2 + 1] = pixelVec2[1];
+    // Velocity
+    if (seaVelVec2[0] !== undefined)
+      velocityVertices[i*numVerticesPath] = Math.sqrt(seaVelVec2[0]*seaVelVec2[0] + seaVelVec2[1]*seaVelVec2[1]);
+
 
     let step = 20;
-    //numVerticesPath = particles[i].vertices.length/2;
     // Set line
     for (var j = 1; j < numVerticesPath; j++){
       // Step (ideally in lat, long, not in pixels)
@@ -72,29 +122,29 @@ const updateParticles = () => {
       pixelVec2[0]+= seaVelVec2[0] * step || 0; // 0 if there is no data
       pixelVec2[1]-= seaVelVec2[1] * step || 0; // 0 if there is no data
       // Assign values to vertices
-      //particles[i].vertices[j*2] = pixelVec2[0];
-      //particles[i].vertices[j*2+1] = pixelVec2[1];
       vertices[i*numVerticesPath*2 + j*2] = pixelVec2[0];
       vertices[i*numVerticesPath*2 + j*2 + 1] = pixelVec2[1];
       // Get new sea velocity
       seaVelVec2 = getSeaVelAtPixel(pixelVec2, seaVelVec2);
+      // Assign velocity
+      if (seaVelVec2[0] !== undefined)
+        velocityVertices[i*numVerticesPath + j] = Math.sqrt(seaVelVec2[0]*seaVelVec2[0] + seaVelVec2[1]*seaVelVec2[1]);
     }
 
   }
 }
 
 // Find a point with data
-const generatePoint = (pixelVec2, seaVelVec2) => {
+const generatePoint = (pixelVec2, seaVelVec2, callStackNum) => {
   // Generate starting pixel
   pixelVec2 = generateRandomPixel(pixelVec2, map.getViewport().offsetWidth, map.getViewport().offsetHeight);
   // Check if it has data
   // Get sea velocity at pixel
   seaVelVec2 = getSeaVelAtPixel(pixelVec2, seaVelVec2);
   // If pixel does not contain data
-  if (seaVelVec2[0] === undefined){
-    seaVelVec2[0] = 0;
-    seaVelVec2[1] = 0;
-    //generatePoint(pixelVec2, seaVelVec2);
+  if (seaVelVec2[0] === undefined && callStackNum < 20){
+    callStackNum = callStackNum || 1;
+    generatePoint(pixelVec2, seaVelVec2, callStackNum); // Recursive function
   }
 }
 
@@ -109,6 +159,13 @@ const generateRandomPixel = (out, viewPortX, viewportY) => {
 }
 
 
+const onMapChange = () => {
+
+  canvasParticles.width = map.getViewport().offsetWidth;
+  canvasParticles.height = map.getViewport().offsetHeight;
+
+}
+
 
 
 
@@ -116,38 +173,118 @@ const generateRandomPixel = (out, viewPortX, viewportY) => {
 // START
 const start = () => {
   canvasParticles = document.getElementById("animationCanvas");
-  canvasParticles.width = map.getViewport().offsetWidth;
-  canvasParticles.height = map.getViewport().offsetHeight;
   ctx = canvasParticles.getContext("2d");
 
-  createParticles();
-  updateParticles();
+  prevTime = performance.now();
+
+  // Update particles and canvas size
+  onMapChange();
+
+  // Define map events
+  // Update canvas and positions
+  map.on('moveend', () => {
+    getDataFromLayers();
+    onMapChange();
+  });
+  // Clear canvas
+  map.on('movestart', () => {
+    ctx.clearRect(0,0, canvasParticles.width, canvasParticles.height);
+    imgDataEast = undefined;
+  });
+
 }
 
 
-// UPDATE
+// UPDATE/LOOP/DRAW
+let prevPosX = [];
+let prevPosY = [];
 const update = () => {
+  // Time differential
+  let timeNow = performance.now();
+  let dt = (timeNow - prevTime)/1000; // in seconds
+
+  // If data is loading
+  if (imgDataEast === undefined || imgDataNorth === undefined)
+    return;
+
+  // Clear rect
+  //ctx.clearRect(0,0, canvasParticles.width, canvasParticles.height);
+
+
+  // Trail effect
+  // https://codepen.io/Tyriar/pen/BfizE
+  ctx.fillStyle = 'rgba(255, 255, 255, .05)';
+  ctx.globalCompositeOperation = "destination-in";
+  ctx.fillRect(0,0, canvasParticles.width, canvasParticles.height);
+  ctx.globalCompositeOperation = "source-over";
+  // Trail color
+  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
 
   // Update paint particle's path
-  //for (var i = 0; i < particles.length; i++){
   for (var i = 0; i< numParticles; i++){
-    //particles[i].life++;
+    // Draw trail
+    // Get previous point in path
+    lifePoint = lifeParticle[i];
+    // Update life
+    lifeParticle[i] += 0.005 + speedFactor*0.1*velocityVertices[i * numVerticesPath + Math.round(lifePoint*numVerticesPath)]; // Decide life increase according to velocity
+
+    if (lifeParticle[i] > 1){
+      lifeParticle[i] = 0;
+    }
+    // Get exact position
+    let prevVertPath = Math.floor(lifeParticle[i]*(numVerticesPath-1)); // From 0 to numVerticesPath
+    let nextVertPath = Math.ceil(lifeParticle[i]*(numVerticesPath-1)); // From 0 to numVerticesPath
+
+    // Interpolation value
+    let interpCoeff = (nextVertPath - lifeParticle[i]*(numVerticesPath-1));
+    let xPos = interpCoeff * vertices[i*numVerticesPath*2 + prevVertPath*2]  +
+                (1-interpCoeff) * vertices[i*numVerticesPath*2 + nextVertPath*2];
+    let yPos = interpCoeff * vertices[i*numVerticesPath*2 + prevVertPath*2 + 1]  +
+                (1-interpCoeff) * vertices[i*numVerticesPath*2 + nextVertPath*2 + 1];
+
+    // First iteration
+    if (lifeParticle[i] == 0){
+      prevPosX[i] = xPos;
+      prevPosY[i] = yPos;
+    }
+
+    ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
+    ctx.beginPath();
+    ctx.moveTo(prevPosX[i], prevPosY[i])
+    ctx.lineTo(xPos, yPos);
+    ctx.stroke();
+
+
+    prevPosX[i] = xPos;
+    prevPosY[i] = yPos;
+/*
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.moveTo(xPos, yPos);
+    ctx.arc(xPos, yPos, 2, 0, Math.PI*2, true);
+    ctx.fill();
+*/
+
+
+
     // Paint particle's path
-    //numVerticesPath = particles[i].vertices.length/2;
-    for (var j = 0; j< numVerticesPath; j++){
-      //if (particles[i].vertices[j*2] == -1) // Non existant data
-      //  continue;
+    /*for (var j = 0; j< numVerticesPath; j++){
+
+      // Full line
       if (j == 0){ // first sample
         ctx.beginPath();
-        //ctx.moveTo(particles[i].vertices[j*2], particles[i].vertices[j*2+1]);
         ctx.moveTo(vertices[i*numVerticesPath*2 + j*2], vertices[i*numVerticesPath*2 + j*2 + 1]);
       } else if (j != 0 && j < numVerticesPath - 1){ // path samples
-        //ctx.lineTo(particles[i].vertices[j*2], particles[i].vertices[j*2+1]);
         ctx.lineTo(vertices[i*numVerticesPath*2 + j*2], vertices[i*numVerticesPath*2 + j*2 + 1]);
       } else if (j == numVerticesPath - 1) { // last sample
         ctx.stroke();
       }
-    }
+    }*/
   }
 
+
+  // Update timer
+  prevTime = timeNow;
+  window.requestAnimationFrame(update);
 }
+window.requestAnimationFrame(update);
--- geoportal/map_utils/mapLayers.js
+++ geoportal/map_utils/mapLayers.js
@@ -642,6 +642,7 @@ const seaVelocityEastLayer = new ol.layer.Tile({
   source: new ol.source.TileWMS ({
     url: 'https://nrt.cmems-du.eu/thredds/wms/med-cmcc-cur-an-fc-d',
     attributions: '© CMEMS ',
+    cacheSize: 500,
     crossOrigin: 'anonymous',
     params: {'LAYERS': 'uo', 'COLORSCALERANGE': seaVelocityColorRange, 'STYLES': 'boxfill/greyscale'}, //'boxfill/occam' or 'boxfill/rainbow'
     // https://nrt.cmems-du.eu/thredds/wms/med-cmcc-cur-an-fc-d?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&TILED=true&COLORSCALERANGE=-0.5354787%2C0.92136043&ELEVATION=-1.0182366371154785&LAYERS=uo&STYLES=boxfill%2Frainbow&TIME=2021-06-22T12%3A00%3A00.000Z&URL=https%3A%2F%2Fnrt.cmems-du.eu%2Fthredds%2Fwms%2Fmed-cmcc-cur-an-fc-d&WIDTH=256&HEIGHT=256&CRS=EPSG%3A4326&BBOX=22.5%2C-11.25%2C33.75%2C0
@@ -657,6 +658,7 @@ const seaVelocityNorthLayer = new ol.layer.Tile({
   source: new ol.source.TileWMS ({
     url: 'https://nrt.cmems-du.eu/thredds/wms/med-cmcc-cur-an-fc-d',
     attributions: '© CMEMS ',
+    cacheSize: 500,
     crossOrigin: 'anonymous',
     params: {'LAYERS': 'vo', 'COLORSCALERANGE': seaVelocityColorRange, 'STYLES': 'boxfill/greyscale'}, //'boxfill/occam' or 'boxfill/rainbow'
     // https://nrt.cmems-du.eu/thredds/wms/med-cmcc-cur-an-fc-d?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&TILED=true&COLORSCALERANGE=-0.5354787%2C0.92136043&ELEVATION=-1.0182366371154785&LAYERS=uo&STYLES=boxfill%2Frainbow&TIME=2021-06-22T12%3A00%3A00.000Z&URL=https%3A%2F%2Fnrt.cmems-du.eu%2Fthredds%2Fwms%2Fmed-cmcc-cur-an-fc-d&WIDTH=256&HEIGHT=256&CRS=EPSG%3A4326&BBOX=22.5%2C-11.25%2C33.75%2C0
