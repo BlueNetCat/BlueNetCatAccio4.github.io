4a85a13e7611b7cb7ad37b47c78406a4bf260afb
Gerard Llorach <gerard.llorach@gmail.com>
2021-08-03 17:57:34 +0200

    testing vue. not so convinced, needs external programs like npm

---
+++ geoportal/menutest/timebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+
+<script>
+//https://www.youtube.com/watch?v=6noJ0dlG7jM&ab_channel=Academind
+// https://www.youtube.com/watch?v=FWQSuskE5UA&ab_channel=Academind
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: "vue component test"
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+</style>
